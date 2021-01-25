29178e2cacf6c6c5b2cf934b28f3778b1c161c95
chrismccreedev <devunity@yahoo.com>
2021-01-25 21:55:01 +0200

    Add `Newtonsoft.Json`. Improve "AsyncHttpRequest`

---
+++ Assets/Scripts/Dirty/ThreadParallelizer/AsyncHttpRequest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Dirty.ThreadParallelizer
+{
+    public static class AsyncHttpRequest
+    {
+        public static async Task<TOutput> Post<TInput, TOutput>(string url, TInput requestModel,
+            string contentType = "application/json", string method = "POST")
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            request.Method = method;
+            request.ContentType = contentType;
+            string requestString = JsonConvert.SerializeObject(requestModel);
+            request.ContentLength = requestString.Length;
+
+            using (StreamWriter writer = new StreamWriter(await request.GetRequestStreamAsync(), Encoding.ASCII))
+                await writer.WriteAsync(requestString);
+
+            return await ReadResponse(request, JsonConvert.DeserializeObject<TOutput>);
+        }
+
+        #region Get
+
+        public static async Task<string> Get(string url, string contentType = "text/plain")
+        {
+            return await ReadResponse(CreateGetRequest(url, contentType));
+        }
+
+        public static async Task<TOutput> Get<TOutput>(string url, Func<string, TOutput> converter, string contentType = "text/plain")
+        {
+            return await ReadResponse(CreateGetRequest(url, contentType), converter);
+        }
+
+        public static async Task<TOutput> GetJson<TOutput>(string url)
+        {
+            return await ReadResponse(CreateGetRequest(url, "application/json"), JsonConvert.DeserializeObject<TOutput>);
+        }
+
+        public static async Task<byte[]> GetByteArray(string url)
+        {
+            return await ReadByteArrayResponse(CreateGetRequest(url, "application/octet-stream"));
+        }
+
+        private static HttpWebRequest CreateGetRequest(string url, string contentType)
+        {
+            HttpWebRequest request = (HttpWebRequest) WebRequest.Create(url);
+            request.ContentType = contentType;
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            
+            return request;
+        }
+
+        #endregion
+
+        #region Response
+
+        private static async Task<string> ReadResponse(HttpWebRequest request)
+        {
+            using (HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+
+        private static async Task<TOutput> ReadResponse<TOutput>(HttpWebRequest request, Func<string, TOutput> converter)
+        {
+            return converter.Invoke(await ReadResponse(request));
+        }
+
+        private static async Task<byte[]> ReadByteArrayResponse(HttpWebRequest request)
+        {
+            using (HttpWebResponse response = (HttpWebResponse) await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                await stream.CopyToAsync(memoryStream);
+
+                return memoryStream.ToArray();
+            }
+        }
+
+        #endregion
+    }
+}
--- Assets/Scripts/Dirty/ThreadParallelizer/FunctionsParallelizerBehaviour.cs
+++ Assets/Scripts/Dirty/ThreadParallelizer/FunctionsParallelizerBehaviour.cs
@@ -1,8 +1,13 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using Evolutex.Evolunity.Extensions;
 using NaughtyAttributes;
 using Proyecto26;
-using ShcherbakUnityAwaiter;
+using RSG;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -19,29 +24,71 @@ namespace Dirty.ThreadParallelizer
         [SerializeField]
         private string fileUrl =
             "https://www.google.com.ua/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png";
-        
-        [Button("Get")]
-        public void Test()
+        [SerializeField]
+        private Slider slider;
+        [SerializeField]
+        private Transform tr;
+
+        private async void Start()
         {
-            RestClient.Get(new RequestHelper {
-                Uri = fileUrl,
-                DownloadHandler = new DownloadHandlerTexture()
-            }).Then(res =>
+            // await Test();
+
+            // ThreadPool.QueueUserWorkItem();
+
+            // var a = new Thread(new ParameterizedThreadStart(async o => await Test()));
+            // a.Start();
+        }
+
+        private void Update()
+        {
+            tr.Translate(Time.deltaTime, 0, 0);
+        }
+
+        [Button("Get Async Client Image")]
+        public async Task TestClientAsync()
+        {
+            HttpClient client = new HttpClient();
+            for (int i = 0; i < count; i++)
             {
-                Image image = Instantiate(imagePrefab, transform);
-                // Texture2D texture = ((DownloadHandlerTexture)res.Request.downloadHandler).texture;
-                //
-                // image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
-                //     new Vector2(texture.width / 2f, texture.height / 2f));
-                image.sprite = res.Request.downloadHandler.data.ToSprite();
-            }).Catch(err => {
-                EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
-            });
-            
-            // for (int i = 0; i < 10; i++)
+                CreateImage(await client.GetByteArrayAsync(fileUrl));
+            }
+        }
+        
+        [Button("Get Async Request Image")]
+        public async Task TestRequestAsync()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                CreateImage(await AsyncHttpRequest.GetByteArray(fileUrl));
+            }
+        }
+
+        private void CreateImage(byte[] bytes)
+        {
+            Image image = Instantiate(imagePrefab, transform);
+
+            Debug.Log("VAR");
+            image.sprite = bytes.ToSprite();
+        }
+
+        public void TestCoroutine()
+        {
+            // IPromise promise = RestClient.Get(new RequestHelper {
+            //     Uri = fileUrl,
+            // }).Then(res =>
             // {
-            //     Task downloadImageTask = HttpRequest.Get<byte[]>()
-            // }
+            //     Image image = Instantiate(imagePrefab, transform);
+            //     // Texture2D texture = ((DownloadHandlerTexture)res.Request.downloadHandler).texture;
+            //     //
+            //     // image.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height),
+            //     //     new Vector2(texture.width / 2f, texture.height / 2f));
+            //     image.sprite = res.Request.downloadHandler.data.ToSprite();
+            // }).Catch(err => {
+            //     EditorUtility.DisplayDialog ("Error", err.Message, "Ok");
+            // }).Progress(progress =>
+            // {
+            //     slider.value = progress;
+            // });
         }
     }
 }
