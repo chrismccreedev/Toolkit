b8a582f5fca3fba268864cd5ec654788dc2bd86f
chrismccreedev <devunity@yahoo.com>
2021-05-12 15:13:00 +0300

    Add dirty extensions. Update submodule

---
+++ Assets/Scripts/Dirty/Extensions/Vexe/ComponentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using Vexe.Runtime.Extensions;
+
+namespace Dirty.Extensions
+{
+    public static class ComponentExtensions
+    {
+        public static Component GetOrAddComponent(this Component c, Type componentType)
+        {
+            return c.gameObject.GetOrAddComponent(componentType);
+        }
+
+        public static T GetOrAddComponent<T>(this Component c) where T : Component
+        {
+            return c.gameObject.GetOrAddComponent<T>();
+        }	
+    }
+}
---
+++ Assets/Scripts/Dirty/Extensions/Vexe/LayerMaskExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vexe.Runtime.Extensions
+{
+	public static class LayerMaskExtensions
+	{
+
+
+	}
+}
