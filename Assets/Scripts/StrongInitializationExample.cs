07c3d0bf2300e9d7d3ba50dab5bfb8c53452b777
chrismccreedev <devunity@yahoo.com>
2021-06-18 20:13:10 +0300

    Add `LunarConsole`

--- Assets/Scripts/StrongInitializationExample.cs
+++ Assets/Scripts/StrongInitializationExample.cs
@@ -1,33 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+// You can extract all initialization logic to a generic parent class, but you will lose member names of components.
 [RequireComponent(typeof(Image), typeof(AspectRatioFitter))]
 public class StrongInitializationExample : MonoBehaviour
 {
     [HideInInspector]
-    // [SerializeField] used to save reference between play mode changes (domain reloading).
+    // [SerializeField] used to save the reference, which was obtained in the OnValidate() method,
+    // between play mode changes (domain reloading).
     [SerializeField]
     private Image image;
-    
+
     [HideInInspector]
-    // [SerializeField] used to save reference between play mode changes (domain reloading).
+    // [SerializeField] used to save the reference, which was obtained in the OnValidate() method,
+    // between play mode changes (domain reloading).
     [SerializeField]
     private AspectRatioFitter aspectRatioFitter;
-
-    private bool IsInitialized => image && aspectRatioFitter;
+    
+    private bool IsComponentsInitialized => image && aspectRatioFitter;
 
     // To initialize components in edit time.
     // It will reduce redundant GetComponent() calls in runtime to improve performance.
     private void OnValidate()
     {
-        if (!IsInitialized)
-            InitializeComponents();
+        InitializeComponentsIfRequired();
     }
 
     private void Awake()
     {
-        if (!IsInitialized)
-            InitializeComponents();
+        InitializeComponentsIfRequired();
     }
 
     // If you want to execute some logic right after instantiating an object,
@@ -36,24 +37,26 @@ public class StrongInitializationExample : MonoBehaviour
     // and at the same time, this data is filled from another class's Awake().
     private void Start()
     {
-        // If you execute the same logic in Awake(), it can be executed before the necessary data is filled.
+        // If you execute the same logic in Awake(), it may be executed before the necessary data is filled.
         DoSomethingWithComponents();
     }
 
     public void DoSomethingWithComponents()
     {
-        // Initialization check for cases when some logic is used before calling Awake() on uninitialized game objects.
+        // Initialization for cases when some logic is used before calling Awake() on uninitialized game objects.
         // This can happen when an object wasn't enabled after instantiating and was not initialized during edit time.
-        if (!IsInitialized)
-            InitializeComponents();
+        InitializeComponentsIfRequired();
 
         // Some logic.
         aspectRatioFitter.aspectRatio = (float)image.sprite.texture.width / image.sprite.texture.height;
     }
 
-    private void InitializeComponents()
+    private void InitializeComponentsIfRequired()
     {
-        image = GetComponent<Image>();
-        aspectRatioFitter = GetComponent<AspectRatioFitter>();
+        if (!IsComponentsInitialized)
+        {
+            image = GetComponent<Image>();
+            aspectRatioFitter = GetComponent<AspectRatioFitter>();
+        }
     }
 }
