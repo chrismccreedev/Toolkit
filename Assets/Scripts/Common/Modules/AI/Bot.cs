e173de126f75c5d37abef1ab524f270787a721c8
chrismccreedev <devunity@yahoo.com>
2022-04-23 15:48:42 +0300

    Add `AI` module. Update package

---
+++ Assets/Scripts/Common/Modules/AI/Bot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Evolutex.Evolunity.Extensions;
+using AI.Vision;
+
+namespace AI
+{
+    public class Bot : Character
+    {
+        [SerializeField]
+        private AiSmartVision _smartVision;
+
+        private void Update()
+        {
+            _smartVision.LookForCharacters(out Character[] characters);
+
+            Debug.Log(characters.AsString());
+        }
+    }
+}
---
+++ Assets/Scripts/Common/Modules/AI/Vision/AiSmartVision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AI.Vision
+{
+    public class AiSmartVision : AiVision
+    {
+        public void LookForCharacters(out Character[] characters)
+        {
+            LookConical(out IEnumerable<Collider> colliders);
+
+            characters = colliders.Select(x => x.GetComponent<Character>()).ToArray();
+        }
+    }
+}
