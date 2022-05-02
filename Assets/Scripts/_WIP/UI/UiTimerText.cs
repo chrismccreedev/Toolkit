e750605d07e2611d405c74a3bd3b8fcf0bed3f45
chrismccreedev <devunity@yahoo.com>
2022-05-02 04:52:51 +0300

    Update submodule

--- Assets/Scripts/_WIP/UI/UiTimerText.cs
+++ Assets/Scripts/_WIP/UI/UiTimerText.cs
@@ -81,4 +81,83 @@ namespace _WIP.UI
             return $"{minutes:00}:{seconds:00}";
         }
     }
+    
+    // Added TimeUpdateMethod.InternalFromTimer update method.
+    // 
+    // public class UiTimerText : UiElement
+    // {
+    //     public Text timerUi;
+    //     public TimeUpdateMethod UpdateMethod;
+    //
+    //     [SerializeField, ShowIf(nameof(IsTimersUpdate))] protected Timer _timer;
+    //
+    //     public virtual TimerTextHandler TimerTextHandler { protected get; set; } = (textComponent, time) =>
+    //     {
+    //         textComponent.text = ToStringUtility.TimeToMinutesSeconds(time);
+    //     };
+    //     public virtual TimeGetter TimeGetter { protected get; set; }
+    //     public Timer Timer => _timer;
+    //     private bool IsTimersUpdate => UpdateMethod == TimeUpdateMethod.TimerUpdate 
+    //         || UpdateMethod == TimeUpdateMethod.InternalFromTimer;
+    //
+    //     protected virtual void Start()
+    //     {
+    //         if (_timer)
+    //             SubscribeToTimer(_timer);
+    //     }
+    //
+    //     protected virtual void Update()
+    //     {
+    //         if (UpdateMethod == TimeUpdateMethod.InternalFromTimer)
+    //         {
+    //             if (Timer == null)
+    //             {
+    //                 Debug.LogError("In order to use " + nameof(TimeUpdateMethod.InternalFromTimer) + ", " +
+    //                     "it is required that " + nameof(Timer) + " is not null. Use " + nameof(SubscribeToTimer) + 
+    //                     "method to assign a timer");
+    //
+    //                 return;
+    //             }
+    //             
+    //             UpdateTimerText(Timer.RemainingTime);
+    //         }
+    //         else if (UpdateMethod == TimeUpdateMethod.InternalFromGetter)
+    //         {
+    //             if (TimeGetter == null)
+    //             {
+    //                 Debug.LogError("In order to use " + nameof(TimeUpdateMethod.InternalFromGetter) + ", " +
+    //                     "it is required that " + nameof(TimeGetter) + " is not null");
+    //
+    //                 return;
+    //             }
+    //
+    //             UpdateTimerText(TimeGetter.Invoke());
+    //         }
+    //     }
+    //
+    //     public void SubscribeToTimer(Timer timer, bool useTimerUpdate = true)
+    //     {
+    //         _timer = timer;
+    //         _timer.Updated += deltaTime =>
+    //         {
+    //             if (UpdateMethod == TimeUpdateMethod.TimerUpdate)
+    //                 UpdateTimerText(_timer.RemainingTime);
+    //         };
+    //
+    //         UpdateMethod = useTimerUpdate ? TimeUpdateMethod.TimerUpdate : TimeUpdateMethod.InternalFromTimer;
+    //     }
+    //
+    //     public void UpdateTimerText(float time)
+    //     {
+    //         TimerTextHandler.Invoke(timerUi, time);
+    //     }
+    //
+    //     public enum TimeUpdateMethod
+    //     {
+    //         ExternalManual,
+    //         TimerUpdate,
+    //         InternalFromTimer,
+    //         InternalFromGetter
+    //     }
+    // }
 }
